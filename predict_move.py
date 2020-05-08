0584e66a7916f6fba0d9dd90f571432ebb686fa2
Nirmal Suthar <nirmal@zymr.com>
2020-05-08 17:10:34 +0530

    code cleanup

--- predict_move.py
+++ predict_move.py
@@ -1,9 +1,4 @@
-import os
-from datetime import time
-
-import packages.tools as tools
 import tensorflow_chessbot
-from packages import sunfish
 import chess
 import chess.engine
 import pyscreenshot as screen
@@ -23,26 +18,18 @@ def reverse_fen(data):
 def suggest_move(args):
     fen = tensorflow_chessbot.main(args)
     fen = fen if args.active == 'b' else reverse_fen(fen)
-    pos = tools.parseFEN(fen)
-    print(' '.join(pos.board))
-    searcher = sunfish.Searcher()
 
-    # position = chess.Board()
-    # epd_info = position.set_epd(fen)
-    # print("Searching...")
-    # move, score = searcher.search(pos, secs=args.time )
-    # move = move if args.active == 'w' else (119 - move[0], 119 - move[1])
-    # suggestedMove = sunfish.render(move[0]) + sunfish.render(move[1])
-    # return suggestedMove, score
+
     engine = chess.engine.SimpleEngine.popen_uci("/Users/nirmal.s/Documents/Bundles_/stockfish-11-mac/Mac/stockfish-11-64")
     board = chess.Board(fen)
+    print(board.is_game_over())
     limit = chess.engine.Limit(time=args.time)
     result = engine.play(board, limit)
 
     print(result.move)
-
-    return limit, limit
     engine.quit()
+    return result
+
 
 
 screenshot_png = 'screenshot.png'
@@ -56,8 +43,8 @@ args.time = 3
 args.predictor = tensorflow_chessbot.ChessboardPredictor()
 
 while True:
-    im = screen.grab()
-    im.save(screenshot_png)
-    score, suggestedMove = suggest_move(args)
-    #print("Suggested move (score):", suggestedMove, score)
+    #im = screen.grab()
+    #im.save(screenshot_png)
+    result = suggest_move(args)
+    print("Suggested move:", result.move)
     input("Press Enter to continue...")
